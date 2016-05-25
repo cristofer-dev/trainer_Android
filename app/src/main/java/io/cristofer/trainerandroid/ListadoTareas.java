0457cf49f10f52155ea936fd4924b812200fbfdd
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-05-25 12:27:27 -0300

    ADD Nueva Activity ListadoTareas
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/io/cristofer/trainerandroid/ListadoTareas.java
    	new file:   app/src/main/res/layout/activity_listado_tareas.xml

---
+++ app/src/main/java/io/cristofer/trainerandroid/ListadoTareas.java
@@ -0,0 +1,13 @@
+package io.cristofer.trainerandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoTareas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_tareas);
+    }
+}
