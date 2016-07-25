edc8f9f8f35a5f024146253d7632d5e66d053d57
Cristofer [w8] <Cristofer.dev@gmail.com>
2016-07-25 16:09:18 -0300

    ADD FloatingActionButton
    	modified:   .idea/misc.xml
    	modified:   app/src/main/java/io/cristofer/trainerandroid/MainActivity.java
    	modified:   app/src/main/res/layout/activity_main.xml

--- app/src/main/java/io/cristofer/trainerandroid/MainActivity.java
+++ app/src/main/java/io/cristofer/trainerandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package io.cristofer.trainerandroid;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AgregarTarea();
+    }
+
+    public void AgregarTarea(){
+        FloatingActionButton btn_add = (FloatingActionButton) findViewById(R.id.btn_plus);
+        btn_add.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+        });
     }
 }
